a36cd1a74f8b7b233e1861595178bbb79901508a
sallyaz22 <115486590+sallyaz22@users.noreply.github.com>
2024-02-07 23:15:42 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+
+function openMune(){
+    document.querySelector('.contianer').style.left = '0%';
+    document.querySelector('.menu').style.opacity ='1'; 
+}
+function closeMenu(){
+    document.querySelector('.contianer').style.left = '-100%';
+    document.querySelector('.menu').style.opacity ='0';
+}
+
+
